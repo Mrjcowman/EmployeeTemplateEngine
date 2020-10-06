900fd88a4e0208d9080a6ec4a41eb2caefed0247
Jacob Cowan <mrjcowman@gmail.com>
2020-10-06 15:45:33 -0700

    Add validation to prompts and generate html

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const Employee = require("./lib/Employee");
 
 const employees = [];
 
+const validateNumber = (input) => {return /\d+/.test(input)||"Please enter a number";};
+
 // TODO: Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -26,22 +28,39 @@ inquirer.prompt([
     {
         message: "What is the ID number of this team's manager?",
         type: "input",
-        name: "managerID"
+        name: "managerID",
+        validate: validateNumber
     },
     {
         message: "What is the email of this team's manager?",
         type: "input",
-        name: "managerEmail"
+        name: "managerEmail",
+        validate: function(input){
+            return employeePrompts.emailRegex.test(input)||"Please enter a valid email";
+        }
     },
     {
         message: "What is the office number of this team's manager?",
         type: "input",
-        name: "managerOffice"
+        name: "managerOffice",
+        validate: validateNumber
     },
     {
         message: "How many employees are part of this manager's team?",
         type: "input",
-        name: "employeeCount"
+        name: "employeeCount",
+        validate: function(input){
+            let isANumber = validateNumber(input);
+            if(isANumber){
+                if(input>0){
+                    return true;
+                } else {
+                    return "Please enter a number greater than 0";
+                }
+            } else {
+                return "That's not a number!";
+            }
+        }
     }
 ]).then(async (answers)=>{
     employees.push(new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice));
@@ -52,18 +71,22 @@ inquirer.prompt([
         await addEmployee(role);
     }
 
-    console.log(employees);
+    let html = render(employees);
+
+    await fs.readdir("output/", err=>{
+        if(err) fs.mkdirSync("output/");
+    })
+
+    fs.writeFile("output/team.html", html, "utf8", (err)=>{
+        if(err) throw err;
+    })
+
 })
 
-// TODO: After the user has input all employees desired, call the `render` function (required
+// After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// TODO: After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
 // TODO: build employee definition sequence for inquirer, to loop through for each added employee
 async function addEmployee(role){
